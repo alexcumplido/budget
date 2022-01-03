0b1e4db1b1cb5767c018cdab806ba5b664385ee7
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-01-03 17:59:25 +0100

    Create Checkbox component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,35 @@
-function App() {
+  import React, {useState} from 'react';
+
+  export function App() {
+
+  const [checkOne, setcheckOne] = useState(false);
+  const [checkTwo, setcheckTwo] = useState(false);
+  const [checkThree, setcheckThree] = useState(false);
+
+  const handleChangeOne = () => setcheckOne(!checkOne);
+  const handleChangeTwo = () => setcheckTwo(!checkTwo);
+  const handleChangeThree = () => setcheckThree(!checkThree);
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Checkbox label='Layout (400€)' check={checkOne} onChange={handleChangeOne} value={400}/>
+      
+      <Checkbox label='Seo(300€)' check={checkTwo} onChange={handleChangeTwo} value={300}/>
+
+      <Checkbox label='GoogleAdds(200€)' check={checkThree} onChange={handleChangeThree} value={200}/>
     </div>
+
+  );
+};
+
+const Checkbox = ({label, check, onChange, value}) => {
+  return (
+    <label>
+      <input type='checkbox' checked={check} onChange={onChange} value={value}/>
+      {label}
+    </label>
   );
 }
 
-export default App;
