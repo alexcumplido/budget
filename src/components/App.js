d6d53b9edba193f93e5ee0b153c9afa2a305950c
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-01-05 11:59:23 +0100

    Task 2 initial implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const Panel = styled.div`
+  width: 200px;
+  height: 200px;
+  border: 1px solid black;
+`;
 
 export function App() {
 
-  const [checkOne, setcheckOne] = useState(false);
-  const [checkTwo, setcheckTwo] = useState(false);
-  const [checkThree, setcheckThree] = useState(false);
+  const [checkOne, setCheckOne] = useState(false);
+  const [checkTwo, setCheckTwo] = useState(false);
+  const [checkThree, setCheckThree] = useState(false);
+  const [inputOne, setInputOne] = useState(0);
+  const [inputTwo, setInputTwo] = useState(0);
   const [total, setTotal] = useState(0);
 
-  const handleCheck1 = () => setcheckOne(!checkOne);
-  const handleCheck2 = () => setcheckTwo(!checkTwo);
-  const handleCheck3 = () => setcheckThree(!checkThree);
+  const handleCheck1 = () => setCheckOne(!checkOne);
+  const handleCheck2 = () => setCheckTwo(!checkTwo);
+  const handleCheck3 = () => setCheckThree(!checkThree);
+
+  const handleInput1 = (e) => setInputOne(e.target.value);
+  const handleInput2 = (e) => setInputTwo(e.target.value);
   
   function handleTotal(event){
     let evtValue = Number(event.target.value);
@@ -18,35 +30,40 @@ export function App() {
 
   return (
     <>
-    <div>
-      <Checkbox 
-        check={checkOne} 
-        onChange={handleCheck1}
-        onClick={handleTotal}
-        value={400} 
-      >
-      Layout (400€)
-      </Checkbox>
-      
-      <Checkbox 
-        check={checkTwo} 
-        onChange={handleCheck2} 
-        onClick={handleTotal}
-        value={300}
-      > 
-      Seo (300€)
-      </Checkbox>
+      <div>
+        <h3>How we can help you ? </h3>
+        <Checkbox 
+          check={checkOne} 
+          onChange={handleCheck1}
+          onClick={handleTotal}
+          value={400} 
+        >
+          Web deployment (400€)
+        </Checkbox>
+        {checkOne && 
+        <Panel>
+          <input type="number" value={inputOne} onChange={handleInput1}/>
+          <input type="number" value={inputTwo} onChange={handleInput2}/>
+        </Panel>}
+        <Checkbox 
+          check={checkTwo} 
+          onChange={handleCheck2} 
+          onClick={handleTotal}
+          value={300}
+        > 
+          Seo Analysis (300€)
+        </Checkbox>
 
-      <Checkbox 
-        check={checkThree} 
-        onChange={handleCheck3} 
-        onClick={handleTotal}
-        value={200}
-      >
-      GoogleAdds (200€)
-      </Checkbox>
+        <Checkbox 
+          check={checkThree} 
+          onChange={handleCheck3} 
+          onClick={handleTotal}
+          value={200}
+        >
+          Google Adds action (200€)
+        </Checkbox>
     </div>
-  <p>Total price is {total}</p>
+    <p>Total price is {total}</p>
   </>
   );
 };
@@ -54,7 +71,13 @@ export function App() {
 const Checkbox = ({check, onChange, onClick, value, children}) => {
   return (
     <label>
-      <input type='checkbox' checked={check} onChange={onChange} onClick={onClick} value={value}/>
+      <input 
+        type='checkbox' 
+        checked={check} 
+        onChange={onChange} 
+        onClick={onClick} 
+        value={value}
+      />
       {children}
     </label>
   );
