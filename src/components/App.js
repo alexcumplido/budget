12242ae271464fa81fc98b304f35cc551c2efab0
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-01-05 18:30:29 +0100

    Refactored components and created Inputs component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {useState} from 'react';
 import styled from 'styled-components';
 
 const Panel = styled.div`
-  width: 200px;
-  height: 200px;
-  border: 1px solid black;
+  width: 400px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  border: 2px solid #000000;
+  border-radius: 10px;
 `;
 
 export function App() {
@@ -22,63 +27,75 @@ export function App() {
 
   const handleInput1 = (e) => setInputOne(e.target.value);
   const handleInput2 = (e) => setInputTwo(e.target.value);
-  
+
   function handleTotal(event){
-    let evtValue = Number(event.target.value);
-    (event.target.checked) ? setTotal(total+evtValue) : setTotal(total-evtValue)
+    let value = Number(event.target.value);
+    (event.target.checked) ? setTotal(total+value) : setTotal(total-value)
   }
 
   return (
     <>
       <div>
-        <h3>How we can help you ? </h3>
+        <h3>Services </h3>
         <Checkbox 
+          label='Web deployment (400€)'
           check={checkOne} 
           onChange={handleCheck1}
           onClick={handleTotal}
           value={400} 
-        >
-          Web deployment (400€)
-        </Checkbox>
+        />
+
         {checkOne && 
         <Panel>
-          <input type="number" value={inputOne} onChange={handleInput1}/>
-          <input type="number" value={inputTwo} onChange={handleInput2}/>
+          <Input label='num. pages' value={inputOne} onChange={handleInput1}/>
+          <Input label='num. languages' value={inputTwo} onChange={handleInput2}/> 
         </Panel>}
+
         <Checkbox 
+          label='Seo Analysis (300€)'
           check={checkTwo} 
           onChange={handleCheck2} 
           onClick={handleTotal}
           value={300}
-        > 
-          Seo Analysis (300€)
-        </Checkbox>
+        /> 
 
         <Checkbox 
+          label='Google Adds action (200€)'
           check={checkThree} 
           onChange={handleCheck3} 
           onClick={handleTotal}
           value={200}
-        >
-          Google Adds action (200€)
-        </Checkbox>
+        />
+
     </div>
     <p>Total price is {total}</p>
   </>
   );
 };
 
-const Checkbox = ({check, onChange, onClick, value, children}) => {
+const Input = ({label, value, onChange}) => {
   return (
     <label>
-      <input 
-        type='checkbox' 
-        checked={check} 
-        onChange={onChange} 
-        onClick={onClick} 
-        value={value}
-      />
-      {children}
+      {label}
+      <input  type='number' value={value} onChange={onChange} min="0"/>
     </label>
   );
 }
+
+const Checkbox = ({label ,check, onChange, onClick, value}) => {
+  return (
+    <label>
+      <input type='checkbox' checked={check} onChange={onChange} onClick={onClick} value={value}/>
+      {label}
+    </label>
+  );
+}
+
+  // const [inputs, setInputs] = useState({pages: '', languages: ''});
+
+  // function handleInputs(evt) {
+  //   setInputs({
+  //     ...inputs, 
+  //     [evt.target.name]: evt.target.value
+  //   });
+  // }
