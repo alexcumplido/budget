fdbd1c388333494bf8da5e715161ef0ca77f6183
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-01-10 13:05:23 +0100

    Organized code for refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,5 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
-
-const Panel = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 0.5rem;
-  gap: 1rem;
-  border: 1px solid #000000;
-  border-radius: 10px;
-`;
 
 const Form = styled.div`
   width: 50%;
@@ -23,10 +11,6 @@ const Form = styled.div`
   gap: 1rem;
 `;
 
-const InputBotones = styled.div`
-  display: flex;
-`;
-
 export function App() {
 
   let [total, setTotal] = useState(0);
@@ -35,43 +19,15 @@ export function App() {
   const [seo, setSeo] = useState(false);
   const [googleAdds, setGoogleAdds] = useState(false);
 
-  //Inputs useState
-  const [paginas, setPaginas] = useState(0);
-  const [idiomas, setIdiomas] = useState(0);
-
-  useEffect(() => {
-    const costoMaquetar = () => {
-      let costoTotal = 0;
-      let costoChecks = 0;
-
-      costoTotal = (Number(paginas) * Number(idiomas))*30;
-
-      if(maquetar) costoChecks += 400;
-      if(seo) costoChecks += 300
-      if(googleAdds) costoChecks += 200;
-
-      setTotal(costoTotal+costoChecks);
-    }
-    costoMaquetar();
-  }, [paginas, idiomas]);
-
   const handleMaquetar = () => setMaquetar(!maquetar);
   const handleSeo = () => setSeo(!seo);
   const handleGoogleAdds = () => setGoogleAdds(!googleAdds);
-
-  //Inputs handlers
-  const handlePaginas = (event) => {
-    setPaginas(event.target.value);
-  }
-  const handleIdiomas = (event) => {
-    setIdiomas(event.target.value);
-  }
 
   //Checkbox handlers
   function handleChecks(event){
     let checkName = event.target.name;
     let isCheck = event.target.checked;
-    
+
     if(checkName === 'maquetar' && isCheck) {
       setTotal(total+400);
     } else if (checkName === 'maquetar' && !isCheck) {
@@ -92,63 +48,29 @@ export function App() {
 
   }
 
-  const sumarInputs = () => {
-    setPaginas(paginas+1)
-  }
-  const restarInputs = () => {
-    setPaginas(paginas-1)
-  }
-
   return (
     <Form>
       <h3>Services </h3>
-      <Checkbox 
-        label='Web deployment (400€)'
-        name='maquetar' 
-        check={maquetar} 
+      <Checkbox
+        label='Maquetación (400€)'
+        name='maquetar'
+        check={maquetar}
         onChange={handleMaquetar}
         onClick={handleChecks}
       />
-    
-      {maquetar && 
-      <Panel>
-          
-      <InputBotones>
-        <label htmlFor='paginas'>num páginas</label>
-        <button onClick={restarInputs}>-</button>
-        <Input
-          id='paginas' 
-          value={paginas} 
-          onChange={handlePaginas} 
-        />
-        <button onClick={sumarInputs}>+</button>
-      </InputBotones>
 
-      <InputBotones>
-        <label htmlFor='idiomas'>num. idiomas</label>
-        <button onClick={restarInputs}>-</button>
-        <Input 
-          id='idiomas'
-          value={idiomas} 
-          onChange={handleIdiomas}
-        /> 
-        <button onClick={sumarInputs}>+</button>
-      </InputBotones>
-
-      </Panel>}
-
-      <Checkbox 
+      <Checkbox
         label='Seo Analysis (300€)'
-        name='seo' 
-        check={seo} 
-        onChange={handleSeo} 
+        name='seo'
+        check={seo}
+        onChange={handleSeo}
         onClick={handleChecks}
-      /> 
-      <Checkbox 
+      />
+      <Checkbox
         label='Google Adds action (200€)'
-        name='googleAdds' 
-        check={googleAdds} 
-        onChange={handleGoogleAdds} 
+        name='googleAdds'
+        check={googleAdds}
+        onChange={handleGoogleAdds}
         onClick={handleChecks}
         value={200}
       />
@@ -166,14 +88,93 @@ const Checkbox = ({label, name, check, onChange, onClick,}) => {
   );
 }
 
-const Input = ({onChange}) => {
-  return (
-    <label>
-      <input  type='text' onChange={onChange}/>
-    </label>
-  );
-}
+  //INPUTS SECTION
+//IMPORT useEffect
+// const Panel = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   padding: 0.5rem;
+//   gap: 1rem;
+//   border: 1px solid #000000;
+//   border-radius: 10px;
+// `;
+
+// const InputBotones = styled.div`
+//   display: flex;
+// `;
+
+  //Inputs useState
+  // const [paginas, setPaginas] = useState(0);
+  // const [idiomas, setIdiomas] = useState(0);
+
+  // useEffect(() => {
+  //   const costoMaquetar = () => {
+  //     let costoInputs = 0;
+  //     let costoChecks = 0;
+
+  //     costoInputs = (Number(paginas) * Number(idiomas))*30;
+
+  //     if(maquetar) costoChecks += 400;
+  //     if(seo) costoChecks += 300
+  //     if(googleAdds) costoChecks += 200;
+
+  //     setTotal(costoChecks+costoInputs);
+  //   }
+  //   costoMaquetar();
+  // }, [paginas, idiomas]);
+
+  // const handlePaginas = (event) => {
+  //   setPaginas(event.target.value);
+  // }
+  // const handleIdiomas = (event) => {
+  //   setIdiomas(event.target.value);
+  // }
+
+  // const sumarInputs = () => {
+  //   setPaginas(paginas+1)
+  // }
+  // const restarInputs = () => {
+  //   setPaginas(paginas-1)
+  // }
+    /* {maquetar &&
+    <Panel>
+
+    <InputBotones>
+      <label htmlFor='paginas'>num páginas</label>
+      <button onClick={restarInputs}>-</button>
+      <Input
+        id='paginas'
+        value={paginas}
+        onChange={handlePaginas}
+      />
+      <button onClick={sumarInputs}>+</button>
+    </InputBotones>
+
+    <InputBotones>
+      <label htmlFor='idiomas'>num. idiomas</label>
+      <button onClick={restarInputs}>-</button>
+      <Input
+        id='idiomas'
+        value={idiomas}
+        onChange={handleIdiomas}
+      />
+      <button onClick={sumarInputs}>+</button>
+    </InputBotones>
+
+    </Panel>} */
+
+  // const Input = ({onChange}) => {
+  //   return (
+  //     <label>
+  //       <input  type='text' onChange={onChange}/>
+  //     </label>
+  //   );
+  // }
 
 
 
-  
+
+
